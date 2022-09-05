decc825860dc4d7553372844a5cdf828335be1ee
Navneet Prajapati <navneetprajapati26@gmail.com>
2022-09-05 11:09:50 +0530

    Qustion 1 is down

--- arrays2D_day_2_9/qus.java
+++ arrays2D_day_2_9/qus.java
@@ -2,4 +2,23 @@ package arrays2D_day_2_9;
 
 public class qus {
 
+    public static void qus1(int[][] mat) {
+
+        int cunt = 0;
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (mat[i][j] == 7) {
+                    cunt++;
+                }
+            }
+        }
+        System.out.println("number of 7's is " + cunt);
+    }
+
+    public static void main(String[] args) {
+        int mat[][] = { { 4, 7, 8 }, { 8, 8, 7 } };
+        qus1(mat);
+
+    }
+
 }
