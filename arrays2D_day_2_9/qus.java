12e8313afd0085bd5194042d2bbe83bfd0176c9a
Navneet Prajapati <navneetprajapati26@gmail.com>
2022-09-05 11:29:33 +0530

    qustion 2 donw

--- arrays2D_day_2_9/qus.java
+++ arrays2D_day_2_9/qus.java
@@ -15,9 +15,23 @@ public class qus {
         System.out.println("number of 7's is " + cunt);
     }
 
+    // Print out the sum of the numbers inthe second row of the “nums” array.
+    // Example :Input - int[][] nums = { {1,4,9},{11,4,3},{2,2,3} };
+    // Output - 18
+
+    public static void qus2() {
+        int mat[][] = { { 1, 4, 9 }, { 11, 4, 3 }, { 2, 2, 3 } };
+        int sum = 0;
+        for (int i = 0; i < mat.length; i++) {
+            sum += mat[1][i];
+        }
+        System.out.println(sum);
+    }
+
     public static void main(String[] args) {
         int mat[][] = { { 4, 7, 8 }, { 8, 8, 7 } };
         qus1(mat);
+        qus2();
 
     }
 
