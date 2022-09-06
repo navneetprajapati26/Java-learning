ac3a8a79404ce6bb7567ead52025567922aed2fb
Navneet Prajapati <navneetprajapati26@gmail.com>
2022-09-06 13:15:06 +0530

    strting Strings

---
+++ strings_day_6_9/strings.java
@@ -0,0 +1,17 @@
+package strings_day_6_9;
+
+public class strings {
+
+    public static void main(String[] args) {
+        char arr[] = { 'a', 'b', 'c', 'd', 'e' };
+
+        // ############# String declration
+        String str = "abcde";
+        // OR
+        String str2 = new String("abcde");
+
+        // **imp
+        // Strings are I M M U T A B L E
+    }
+
+}
