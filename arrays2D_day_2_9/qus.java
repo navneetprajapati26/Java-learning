0d58fa3072d00d644d2b5ad65dd913b0a1c08fc4
Navneet Prajapati <navneetprajapati26@gmail.com>
2022-09-06 12:41:38 +0530

    transpose matrix down

--- arrays2D_day_2_9/qus.java
+++ arrays2D_day_2_9/qus.java
@@ -1,5 +1,7 @@
 package arrays2D_day_2_9;
 
+import java.util.Scanner;
+
 public class qus {
 
     public static void qus1(int[][] mat) {
@@ -28,11 +30,57 @@ public class qus {
         System.out.println(sum);
     }
 
-    public static void main(String[] args) {
-        int mat[][] = { { 4, 7, 8 }, { 8, 8, 7 } };
-        qus1(mat);
-        qus2();
+    // Write a program to Find Transpose of a Matrix.
+    // What is Transpose?
+    // Transpose of a matrix is the process ofswapping therows to columns.
+    // For a 2x3 matrix:-
 
+    // Matrix
+    // a11 a12 a13
+    // a21 a22 a23
+
+    // Transposed Matrix
+    // a11 a21
+    // a12 a22
+    // a13 a23
+
+    public static void printMat(int mat[][]) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        // int i = sc.nextInt();
+        // int j = sc.nextInt();
+        // int mat[][] = new int[i][j];
+
+        // for (int k = 0; k < mat.length; k++) {
+        // for (int l = 0; l < mat[0].length; l++) {
+        // mat[k][l] = sc.nextInt();
+        // }
+        // }
+
+        int mat[][] = { { 1, 2, 3 }, { 4, 5, 6 } };
+        // qus1(mat);
+        // qus2();
+        printMat(mat);
+
+        // Transpose matrix
+        int[][] transpose = new int[mat[0].length][mat.length];
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                transpose[j][i] = mat[i][j];
+            }
+        }
+
+        printMat(transpose);
     }
 
 }
