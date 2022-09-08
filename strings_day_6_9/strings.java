709660fa249ba6bd749c5c3908044dfb5a10f27a
Navneet Prajapati <navneetprajapati26@gmail.com>
2022-09-09 00:36:43 +0530

    importent qustion done

--- strings_day_6_9/strings.java
+++ strings_day_6_9/strings.java
@@ -29,8 +29,9 @@ public class strings {
     }
 
     public static void sortestDis(String str) {
-        int x = 0;
-        int y = 0;
+        float x = 0;
+        float y = 0;
+        int count = 0;
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) == 'w') {
                 x += -1;
@@ -40,12 +41,70 @@ public class strings {
                 y += -1;
             } else if (str.charAt(i) == 'n') {
                 y += 1;
+            } else {
+                count++;
             }
         }
-        int a = (int) Math.pow(x, 2) + (int) Math.pow(y, 2);
+        float a = x * x + y * y; // Math.pow(x, 2) + Math.pow(y, 2);
 
-        int sD = (int) Math.sqrt(a);
-        System.out.println(sD);
+        float sD = (float) Math.sqrt(a);
+        System.out.println("Shortest length " + sD + " and nmumber of non directional instruction " + count);
+    }
+
+    public static void subString(String str, int si, int ei) {
+        String substr = "";
+        for (int i = si; i < ei; i++) {
+            substr += str.charAt(i);
+        }
+
+        printString(substr);
+    }
+
+    // largest
+    public static void lxicalLarg() {
+        String fruits[] = { "apple", "mango", "banana" };
+        String largest = fruits[0];
+        for (int i = 0; i < fruits.length; i++) {
+            if (largest.compareTo(fruits[i]) < 0) {
+                largest = fruits[i];
+            }
+        }
+        System.out.println(largest);
+    }
+
+    public static void impQus_toUpperCase(String str) {
+        StringBuilder sd = new StringBuilder("");
+
+        char ch = Character.toUpperCase(str.charAt(0));
+        sd.append(ch);
+
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i - 1) == ' ') {
+                sd.append(Character.toUpperCase(str.charAt(i)));
+            } else {
+                sd.append(str.charAt(i));
+            }
+        }
+
+        System.out.println(sd);
+
+    }
+
+    public static void impQus_compretionStr(String str) {
+        String newStr = " ";
+        for (int i = 0; i < str.length(); i++) {
+            Integer count = 1;
+            while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+
+            newStr += str.charAt(i);
+            if (count > 1) {
+                newStr += count.toString();
+            }
+        }
+        System.out.println(newStr);
     }
 
     public static void main(String[] args) {
@@ -81,8 +140,13 @@ public class strings {
         // System.out.println(lastNAme.charAt(0) + " love " + firstName.charAt(0));
         // printString(lastNAme);
         String str = sc.nextLine();
-        // palindrome(str);
-        sortestDis(str);
+        // // palindrome(str);
+        // sortestDis(str);
+        // subString(str, 2, 3);
+        // System.out.println(str.substring(2, 3));
+        lxicalLarg();
+        impQus_toUpperCase(str);
+        impQus_compretionStr(str);
 
     }
 
