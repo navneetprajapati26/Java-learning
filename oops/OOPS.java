d7baa38c53f857179f5717928cc2adb33fde4711
Navneet Prajapati <navneetprajapati26@gmail.com>
2022-09-12 11:34:46 +0530

    getter setter down

--- oops/OOPS.java
+++ oops/OOPS.java
@@ -4,17 +4,37 @@ public class OOPS {
     public static void main(String[] args) {
         Pan p1 = new Pan();
         p1.setColer("Blue");
-        System.out.println(p1.coler);
+        // System.out.println(p1.coler);
         p1.setTip(5);
-        System.out.println(p1.tip);
+        // System.out.println(p1.tip);
 
+        // Accese modifir
         BankAccount myAcc = new BankAccount();
         myAcc.userName = "ujala yadav";
         myAcc.setPassword("navneet");
 
+        // getter & setter
+        Notes myNote = new Notes();
+        myNote.setNote("ujju");
+        String n1 = myNote.getNote();
+        System.out.println(n1);
     }
 }
 
+// getter & setter & this -> for rifrens
+class Notes {
+    private String note;
+
+    String getNote() {
+        return this.note;
+    }
+
+    void setNote(String newNote) {
+        this.note = newNote;
+    }
+}
+
+// Accese modifir
 class BankAccount {
 
     public String userName;
