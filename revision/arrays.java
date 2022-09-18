e553daaff797f8514d4164abfba20f01005f6838
Navneet Prajapati <navneetprajapati26@gmail.com>
2022-09-18 12:08:19 +0530

    finding largest and smalest  is complit

--- revision/arrays.java
+++ revision/arrays.java
@@ -12,10 +12,27 @@ public class arrays {
         }
     }
 
+    public static void getLargSmal(int[] arr) {
+        int largest = Integer.MIN_VALUE;
+        int smalestm = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (largest < arr[i]) {
+                largest = arr[i];
+            }
+            if (smalestm > arr[i]) {
+                smalestm = arr[i];
+            }
+        }
+        System.out.println("largest " + largest);
+        System.out.println("smalestm " + smalestm);
+    }
+
     public static void main(String[] args) {
-        int arr[] = { 2, 2, 3, 4 };
+        int arr[] = { 2, 1, 3, 4 };
         int kay = 2;
-        linerSharch(arr, kay);
+        // linerSharch(arr, kay);
+        getLargSmal(arr);
     }
 
 }
