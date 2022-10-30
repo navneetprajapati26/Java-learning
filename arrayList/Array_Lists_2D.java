048adf332d1bbd887713d4237e5bb76444237c50
Navneet Prajapati <navneetprajapati26@gmail.com>
2022-10-31 00:42:42 +0530

    pair sum brute forc approch

--- arrayList/Array_Lists_2D.java
+++ arrayList/Array_Lists_2D.java
@@ -102,7 +102,37 @@ public class Array_Lists_2D {
         hight.add(8);
         hight.add(3);
         hight.add(7);
-        System.out.println("Max Water is " + maxWater(hight));
-        System.out.println("Max Water is " + maxWAter_tow_pointer_approch(hight));
+        // System.out.println("Max Water is " + maxWater(hight));
+        // System.out.println("Max Water is " + maxWAter_tow_pointer_approch(hight));
     }
+
+    // Imp. Qus. # pair sum brute forc approch
+    public static boolean pairSum(ArrayList<Integer> list, int target) {
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i) + list.get(j) == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Imp. Qus. # pair Sum implimantetion
+    public static void pairSumImp() {
+        ArrayList<Integer> list = new ArrayList<>();
+        // 1 8 6 2 5 4 8 3 7
+        list.add(1);
+        list.add(8);
+        list.add(6);
+        list.add(2);
+        list.add(5);
+        list.add(4);
+        list.add(8);
+        list.add(3);
+        list.add(7);
+        System.out.println(pairSum(list, 9));
+        // System.out.println("Max Water is " + maxWAter_tow_pointer_approch(hight));
+    }
+
 }
