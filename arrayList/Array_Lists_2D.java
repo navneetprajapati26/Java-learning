20f036ba02304552e2ce20a644aaf5ae437079ac
Navneet Prajapati <navneetprajapati26@gmail.com>
2022-11-01 11:56:33 +0530

    Pair Sum2 Imp. Qus. # pair sum2 (here ArrayList sorted but in rotation mainer)

--- arrayList/Array_Lists_2D.java
+++ arrayList/Array_Lists_2D.java
@@ -36,7 +36,8 @@ public class Array_Lists_2D {
         // TrevIn2D(mainList);
 
         // maxWaterImp();
-        pairSumImp();
+        // pairSumImp();
+        pairSum2Imp();
 
     }
 
@@ -158,4 +159,52 @@ public class Array_Lists_2D {
 
     }
 
+    // Imp. Qus. # pair sum2 (here ArrayList sorted but in rotation mainer)
+    public static boolean pairSum2(ArrayList<Integer> list, int target) {
+
+        int bp = -1;
+        int n = list.size();
+        for (int i = 0; i < list.size(); i++) {// breking point
+            if (list.get(i) > list.get(i + 1)) {
+                bp = i;
+                break;
+            }
+        }
+        int lp = bp + 1;// smallest
+        int rp = bp;
+
+        while (lp != rp) {
+            // case 1
+            if (list.get(lp) + list.get(rp) == target) {
+                return true;
+
+            }
+
+            // case 2
+            if (list.get(lp) + list.get(rp) == target) {
+                lp = (lp + 1) % n;
+            }
+            // case 3
+            else {
+                rp = (n + rp - 1) % n;
+            }
+        }
+
+        return false;
+
+    }
+
+    // Imp. Qus. # pair Sum2 implimantetion
+    public static void pairSum2Imp() {
+        ArrayList<Integer> list = new ArrayList<>();
+        // 11 15 6 8 9 10
+        list.add(11);
+        list.add(15);
+        list.add(6);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+        System.out.println(pairSum2(list, 16));
+
+    }
 }
