d8a6f6f2cb16f86d9f43b74903b698eb0df6704a
Navneet Prajapati <navneetprajapati26@gmail.com>
2022-11-01 13:08:06 +0530

    Bit Wise 'OR (|)' optreter

--- bitManipuletion/bitManup.java
+++ bitManipuletion/bitManup.java
@@ -15,5 +15,17 @@ public class bitManup {
         // 6= 110
         // ----------
         // result 100
+
+        // # importent Bit Wise 'OR (|)' optreter
+        // 0 | 0 = 0
+        // 0 | 1 = 1
+        // 1 | 0 = 1
+        // 1 | 1 = 1
+        System.out.println("Bit Wise 'OR (|)' optreter " + (5 | 6));
+        // 5 | 6,
+        // 5= 101
+        // 6= 110
+        // ----------
+        // result 111
     }
 }
