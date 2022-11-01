f815724595acf376c62765a134eaef9e07a2db0e
Navneet Prajapati <navneetprajapati26@gmail.com>
2022-11-01 13:02:42 +0530

    Bit Wise 'AND' optreter

---
+++ bitManipuletion/bitManup.java
@@ -0,0 +1,19 @@
+package bitManipuletion;
+
+import java.util.*;
+
+public class bitManup {
+    public static void main(String[] args) {
+        // # importent Bit Wise 'AND' optreter
+        // 0 & 0 = 0
+        // 0 & 1 = 0
+        // 1 & 0 = 0
+        // 1 & 1 = 1
+        System.out.println(5 & 6);
+        // 5 % 6,
+        // 5= 101
+        // 6= 110
+        // ----------
+        // result 100
+    }
+}
