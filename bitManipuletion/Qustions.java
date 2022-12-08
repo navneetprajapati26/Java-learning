17b692c32621928845d2a68311554327e7826112
Navneet Prajapati <navneetprajapati26@gmail.com>
2022-12-08 19:12:21 +0530

    doing qus

---
+++ bitManipuletion/Qustions.java
@@ -0,0 +1,76 @@
+package bitManipuletion;
+
+ class Qustions {
+//     int n ,k;
+//     Qustions(int n, int k){
+//         this.n = n;
+//         this.k = k;
+//     }
+
+//    todo: Q 1. )Check if the ith bit is set or not
+     public String  isKthBitSet(int n, int k){
+     int bitMask = 1<<(k-1);
+     if ((n & bitMask) != 0){
+         return  "set";
+     }
+     else {
+         return  "not set";
+     }
+     }
+
+     // todo: Q 2.) Set the ith bit of a number.
+     public  void  setKthBit(int n, int k){
+         int bitMske = 1<<(k-1);
+         System.out.println(n|bitMske);
+     }
+
+     // todo: Q 3.) clear the ith bit of a number
+
+     public  void  clearKthBit(int n, int k){
+         int bitMask = ~(1<<(k-1));
+         System.out.println(n&bitMask);
+     }
+
+     // todo: Q 4.) Remove the last set bit of a number
+
+     public  void removeLastSetBit(int n){
+         int bitMask = n-1;
+         System.out.println(n&bitMask);
+     }
+
+     //todo: finding possition of last set bit
+     public  void positionOfLastSet(int n){
+         int bitMask = ~(n-1);
+         System.out.println(n&bitMask);
+     }
+
+     // todo: Q 5. ) Find whether a number is even or odd
+     public void  even_or_odd(int n){
+         int bitMask =1;
+         if ((n&bitMask) == 0){
+             System.out.println("even");
+         }
+         else {
+             System.out.println("odd");
+         }
+     }
+     public  void  usingdivMod(int n){
+         int calcnum = (n/2)*2;
+         if(calcnum == n){
+             System.out.println("even");
+         }
+         else {
+             System.out.println("odd");
+         }
+     }
+     public void  using_rs_ls(int n){
+         int bitMsak = (n>>1)<<1;
+         if(n == bitMsak){
+             System.out.println("even");
+         }
+         else {
+             System.out.println("odd");
+         }
+     }
+
+}
---
+++ bitManipuletion/Run_solution.java
@@ -0,0 +1,19 @@
+package bitManipuletion;
+
+public class Run_solution {
+    public static void main(String[] args) {
+
+        Qustions q = new Qustions();
+        //System.out.println(q.isKthBitSet(13,3));
+
+        //q.setKthBit(13,2);
+        //q.clearKthBit(13,4);
+        //q.removeLastSetBit(214);
+        //q.positionOfLastSet(3);
+        //q.even_or_odd(5);
+        //q.usingdivMod(5);
+        //q.using_rs_ls(5);
+    }
+}
+
+
